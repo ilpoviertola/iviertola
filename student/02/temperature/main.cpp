30efc9bf1ced75c86af5be42f793177471b8a9a0
Ilpo Viertola <ilpo.viertola@tuni.fi>
2019-09-12 13:02:22 +0300

    Fixed filepath

---
+++ student/02/temperature/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int temp = 0;
+    cout << "Enter a temperature: ";
+    cin >> temp;
+    // Write your code here
+    cout << temp << " degrees Celsius is " << temp * 1.8 + 32 << " degrees Fahrenheit" << endl;
+    cout << temp << " degrees Fahrenheit is " << (temp-32) / 1.8  << " degrees in Celsius" << endl;
+    return 0;
+}
