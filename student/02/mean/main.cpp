895f50ef1155b852d78dfbe2fb4a4313df752955
Ilpo Viertola <ilpo.viertola@tuni.fi>
2019-09-12 14:31:25 +0300

    First submission

---
+++ student/02/mean/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Write here a function counting the mean value
+
+int main()
+{
+    int nroAmount = 0;
+    std::cout << "From how many integer numbers you want to count the mean value? ";
+    std::cin >> nroAmount;
+    if(nroAmount > 0){
+        int i = 1;
+        int sum = 0;
+        while(i <= nroAmount){
+            int input = 0;
+            std::cout << "Input " << i << ". number: ";
+            std::cin >> input;
+            sum = input + sum;
+            ++i;
+        }
+        std::cout << "Mean value of the given numbers is " << sum << std::endl;
+    }
+    else{
+        std::cout << "Cannot count mean value from " << nroAmount << " numbers" << std::endl;
+    }
+}
