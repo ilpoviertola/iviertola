f1290db41be69dc327e73ca2ddbcb041d092bdd2
Ilpo Viertola <ilpo.viertola@tuni.fi>
2019-09-12 13:04:41 +0300

    Fixed output

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -9,6 +9,6 @@ int main()
     cin >> temp;
     // Write your code here
     cout << temp << " degrees Celsius is " << temp * 1.8 + 32 << " degrees Fahrenheit" << endl;
-    cout << temp << " degrees Fahrenheit is " << (temp-32) / 1.8  << " degrees in Celsius" << endl;
+    cout << temp << " degrees Fahrenheit is " << (temp-32) / 1.8  << " degrees Celsius" << endl;
     return 0;
 }
