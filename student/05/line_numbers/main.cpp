193e596daeef63407a81dcf69c0da19d3731c60e
Ilpo Viertola <ilpo.viertola@tuni.fi>
2019-10-04 21:51:37 +0300

    ./3_1 subm: Full program

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int readWriteFile(std::string inputFile, std::string outputFile)
+{
+    std::ifstream fileObj(inputFile);
+    std::ofstream fileObjW(outputFile);
+
+    if(fileObj){
+
+        int rowNmbr = 1;
+        std::string row;
+
+        while(getline(fileObj, row)){
+            row.insert(0, std::to_string(rowNmbr)+" ");
+            fileObjW << row << std::endl;
+            rowNmbr += 1;
+        }
+
+        fileObj.close();
+        fileObjW.close();
+
+
+    } else {
+
+        std::cout << "Error! The file " << inputFile << " cannot be opened." << std::endl;
+        return(EXIT_FAILURE);
+
+    }
+
+    return 0;
+
+}
+
+int main()
+{
+    std::string inputFile;
+    std::string outputFile;
+
+    std::cout << "Input file: ";
+    getline(std::cin, inputFile);
+    std::cout << "Output file: ";
+    getline(std::cin, outputFile);
+
+    readWriteFile(inputFile, outputFile);
+
+}
