0b25cd5c48a9aaecc363a4e4d9d7da9d94bdc57c
Ilpo Viertola <ilpo.viertola@tuni.fi>
2019-11-27 10:32:31 +0200

    full prog

---
+++ student/12/bmi/mainwindow.cpp
@@ -0,0 +1,42 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->countButton, SIGNAL(clicked(bool)), this, SLOT(calculate_bmi()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::calculate_bmi()
+{
+    QString weight_qstr = ui->weightLineEdit->text();
+    QString height_qstr = ui->heightLineEdit->text();
+
+    if(not height_qstr.isEmpty() && not weight_qstr.isEmpty()){
+        ui->infoTextBrowser->clear();
+
+        double weight = weight_qstr.toDouble();
+        double height = height_qstr.toDouble() / 100;
+
+        double bmi = weight / ( height * height );
+        QString bmi_qstr = QString::number(bmi);
+
+        ui->resultLabel->setText(bmi_qstr);
+
+        if( bmi > 0 && bmi <= 18.5 ){
+            ui->infoTextBrowser->append(QString::fromStdString("You are underweight."));
+        } else if ( bmi > 18.5 && bmi < 25 ){
+            ui->infoTextBrowser->append(QString::fromStdString("You are normal weight."));
+        } else if ( bmi >= 25 ){
+            ui->infoTextBrowser->append(QString::fromStdString("You are overweight."));
+        }
+    }
+}
